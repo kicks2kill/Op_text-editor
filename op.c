b1bd52547d6993c95b367f3bda82d06e184e9d71
Orinxlm <w.spicerx@gmail.com>
2020-12-08 20:21:04 -0500

    arrow keys and page up/down are now introduced

--- op.c
+++ op.c
@@ -34,6 +34,8 @@ enum editorKey {
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN
 };
 
 //  // TERMINAL
@@ -76,6 +78,7 @@ void enableRawMode()
 }
 
 /* wait for one keypress and return it. */
+
 int editorReadKey() 
 {
     int nread;
@@ -94,13 +97,27 @@ int editorReadKey()
         if(read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
         if(seq[0] == '[') {
-            switch(seq[1]){
-                case 'A' : return ARROW_UP;
-                case 'B' : return ARROW_DOWN;
-                case 'C' : return ARROW_RIGHT;
-                case 'D' : return ARROW_LEFT;
+            if(seq[1] >= '0' && seq[1] <= '9') {
+                if(read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if(seq[2] == "~") {
+                    switch(seq[1]) {
+                        /*page up is sent as <esc>[5~ and page down as <esc>[6~   
+                        seq needs to be able to store 3 bytes. 
+                        if the byte after [ is a digit, we read another byte expecting it to be a ~. 
+                        */
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                    }
+                }
+            } else {
+                switch(seq[1]){
+                    case 'A' : return ARROW_UP;
+                    case 'B' : return ARROW_DOWN;
+                    case 'C' : return ARROW_RIGHT;
+                    case 'D' : return ARROW_LEFT;
             }
         }
+    }
         return '\x1b';
     } else {
         return c;
@@ -191,7 +208,14 @@ void editorProcessKeypress()
             write(STDOUT_FILENO, "\x1b[H",3);
             exit(0);
             break;
-
+        case PAGE_UP:
+        case PAGE_DOWN:
+        {
+            int times = E.screencols;
+            while(times--)
+                editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+        }
+        break;
         case ARROW_UP:
         case ARROW_DOWN:
         case ARROW_LEFT:
@@ -205,16 +229,24 @@ void editorMoveCursor(int key)
 {
     switch(key){
         case ARROW_LEFT:
-            E.cx--;
+            if(E.cx != 0) {
+                E.cx--;    
+            }
             break;
         case ARROW_RIGHT:
-            E.cx++;
+            if(E.cx != E.screencols - 1) {
+                E.cx++;
+            }
             break;
         case ARROW_UP:
-            E.cy--;
+            if(E.cy != 0) {
+                E.cy--;
+            }
             break;
         case ARROW_DOWN:
-            E.cy++;
+            if(E.cy != E.screenrows - 1) {
+                E.cy++;
+            }
             break;
     }
 }
